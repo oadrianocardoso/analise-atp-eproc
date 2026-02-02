95f2b430cba796e48ced37e8794b8959945deda2
oadrianocardoso <oadrianocardoso@gmail.com>
2026-02-02 17:26:18 -0300

    Add files via upload

---
+++ analise-atp-eproc.user.js.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Análise de ATP eProc (Loader LOCAL Modular)
+// @namespace    https://tjsp.eproc/automatizacoes
+// @version      9.0.28-modular-local
+// @description  Loader modular local do Análise de ATP eProc
+// @author       ADRIANO AUGUSTO CARDOSO E SANTOS
+// @run-at       document-start
+// @noframes
+// @match        https://eproc1g.tjsp.jus.br/eproc/*
+// @match        https://eproc-1g-sp-hml.tjsp.jus.br/*
+// @grant        none
+
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/utils.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/02_dom_and_style.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/03_parsing_rules.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/extract_dados.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/extract_fluxos.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/04_fluxos_text_and_zip.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/05_bpmn_build_export.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/06_conflicts_ui_init_and_close.js
+// @require      file:///C:/Users/Adriano/Downloads/ATP_MODULAR/01_bpmn_modal_and_core.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  console.log('[ATP][LOADER] Loader LOCAL modular ativo (9.0.28-modular-local)');
+})();
